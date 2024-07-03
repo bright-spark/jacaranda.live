ddfecc0a86374b198bd23eb1e56bb3b4e5d66a95
martin <hex0h@outlook.com>
2024-07-03 06:58:37 +0200

    14

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "sw-v1.0.13";
+const CACHE = "sw-v1.0.14";
 
 const offlineFallbackPage = "offline.html";
 
